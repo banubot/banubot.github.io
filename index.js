64c90624e2d0ec4f91b54524886cfa7f281828c5
Hannah Parraga <hannahparraga@email.arizona.edu>
2017-10-02 20:11:51 -0700

    ready to rumble

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-var fs = require('fs')
-const server = require('diet');
+var fs = require('fs');
+var path = require('path');
+
+var server = require('diet');
 function main () {
 	var app = server();
 	fs.stat(app.path+'/dev',function(err,stat){
@@ -7,16 +9,15 @@ function main () {
 			app.listen('localhost:8000');
 
 		} else {
-			app.listen('banubot.com')
+			app.listen('banubot.com');
 			app.error(function($){
-				$.end('ERROR500')
-			})
+				$.end('ERROR500');
+			});
 		}
-	})
-	console.log(app.path)
+	});
 	// allows you to use $.html(app.path + '/path/to/file')
 	// to render pages using ect templating
-	const ect = require('diet-ect')({ path: app.path+'/views/' });
+	var ect = require('diet-ect')({ path: app.path+'/views/' });
 	app.header(ect);
 
 	// serves static files like css, javascript and images
@@ -31,12 +32,12 @@ function main () {
 
 	// extremely low level way to serve a web page
 	app.get('/buns/', function($){
-		$.header('content-type', 'text/html') // tells client to render sent page as HTML
+		$.header('content-type', 'text/html'); // tells client to render sent page as HTML
 		fs.readFile(app.path+'/static/buns/index.html', 'utf8', function (err,data) {
 			if (err) throw err;
-			$.end(data)
+			$.end(data);
 		});
-	})
+	});
 
 	// reads in scores "database" and sends it to the client
 	app.get('/buns/highscore/', function($){
@@ -47,16 +48,49 @@ function main () {
 			$.data = JSON.parse(data);
 			$.json();
 			$.end();
-		})
+		});
 	});
+	
+	app.get('/kgb/flashcards/:deck', function($){
+		fs.readFile(path.join(app.path, 'flashcards', $.params.deck),'utf8',function(err,data){
+			if (err) {
+				throw err;
+			}
+			var array = data.split('\n').map(function(line){
+				line = line.split(',');
+				return {
+					front: line[1], 
+					back: line[0]
+				};				
+			});
+			array.pop(); //That's janky! The last card comes in as undef
+			//bc of \n in last row
+			$.data = array;
+			$.json();
+			$.end();
+		});
+	});
+	
+	app.get('/kgb/flashcards/', function($){
+		fs.readdir(path.join(app.path, 'flashcards'),function(err,files){
+			if (err) {
+				throw err;
+			}
+			$.data = files;
+			$.json();
+			$.end();
+
+		});
+	});
+
 
 	// reads in scores database, attempts to insert a new score and rewrites scores database
 	app.post('/buns/highscore/', function($){
 		fs.readFile(app.path+'/buns/scores.json','utf8',function(err,data){
 			if (err) throw err;
-			const scores = JSON.parse(data);
-			const new_score = $.body.score;
-			const name = $.body.name;
+			var scores = JSON.parse(data);
+			var new_score = $.body.score;
+			var name = $.body.name;
 			scores.push({
 				name: name,
 				score: new_score 
@@ -64,17 +98,17 @@ function main () {
 			scores.sort(function (a, b) {
 				return a.score - b.score;
 			});
-			const new_scores = scores.slice(5);
+			var new_scores = scores.slice(5);
 			fs.writeFile(app.path+'/buns/scores.json', JSON.stringify(new_scores), 'utf8', function(err) {
 				if (err) throw err;
 				$.success();
 				$.end();
-			})
+			});
 		});
 	});
 }
 module.exports = main;
 
 if (require.main === module) {
-	main()
+	main();
 }
