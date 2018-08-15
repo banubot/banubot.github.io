d11f2b20ed9d785077f48deca44ffe528c2175a3
Hannah Parraga <hannahparraga@email.arizona.edu>
2018-08-14 19:25:43 -0700

    update mime

--- index.js
+++ index.js
@@ -39,19 +39,7 @@ app.get('/buns/', function($){
 	});
 });
 
-// reads in scores "database" and sends it to the client
-app.get('/buns/highscore/', function($){
-	fs.readFile(app.path+'/buns/scores.json','utf8',function(err,data){
-		if (err) {
-			throw err;
-		}
-		$.data = JSON.parse(data);
-		$.json();
-		$.end();
-	});
-});
-
-app.get('/kgb/flashcards/:deck', function($){
+app.get('/flashcards/cards/:deck', function($){
 	fs.readFile(path.join(app.path, 'flashcards', $.params.deck),'utf8',function(err,data){
 		if (err) {
 			throw err;
@@ -71,7 +59,7 @@ app.get('/kgb/flashcards/:deck', function($){
 	});
 });
 
-app.get('/kgb/flashcards/', function($){
+app.get('/flashcards/cards/', function($){
 	fs.readdir(path.join(app.path, 'flashcards'),function(err,files){
 		if (err) {
 			throw err;
@@ -80,93 +68,6 @@ app.get('/kgb/flashcards/', function($){
 		$.json();
 		$.end();
 
-	});
-});
-app.get('/cs/flashcards/', function($){
-	fs.readdir(path.join(app.path, 'static/cs/cards'),function(err,files){
-		if (err) {
-			throw err;
-		}
-		$.data = files;
-		$.json();
-		$.end();
-
-	});
-});
-
-app.get('/cs/flashcards/:deck', function($){
-	fs.readFile(path.join(app.path, 'static/cs/cards', $.params.deck),'utf8',function(err,data){
-		if (err) {
-			throw err;
-		}
-		var array = data.split('\n').map(function(line){
-			line = line.split('|');
-			return {
-				front: line[1], 
-				back: line[0]
-			};				
-		});
-		array.pop(); //That's janky! The last card comes in as undef
-		//bc of \n in last row
-		$.data = array;
-		$.json();
-		$.end();
-	});
-});
-
-
-app.get('/math/flashcards/', function($){
-	fs.readdir(path.join(app.path, 'static/math/cards'),function(err,files){
-		if (err) {
-			throw err;
-		}
-		$.data = files;
-		$.json();
-		$.end();
-
-	});
-});
-
-app.get('/math/flashcards/:deck', function($){
-	fs.readFile(path.join(app.path, 'static/math/cards', $.params.deck),'utf8',function(err,data){
-		if (err) {
-			throw err;
-		}
-		var array = data.split('\n').map(function(line){
-			line = line.split('|');
-			return {
-				front: line[1], 
-				back: line[0]
-			};				
-		});
-		array.pop(); //That's janky! The last card comes in as undef
-		//bc of \n in last row
-		$.data = array;
-		$.json();
-		$.end();
-	});
-});
-
-// reads in scores database, attempts to insert a new score and rewrites scores database
-app.post('/buns/highscore/', function($){
-	fs.readFile(app.path+'/buns/scores.json','utf8',function(err,data){
-		if (err) throw err;
-		var scores = JSON.parse(data);
-		var new_score = $.body.score;
-		var name = $.body.name;
-		scores.push({
-			name: name,
-			score: new_score 
-		});
-		scores.sort(function (a, b) {
-			return a.score - b.score;
-		});
-		var new_scores = scores.slice(5);
-		fs.writeFile(app.path+'/buns/scores.json', JSON.stringify(new_scores), 'utf8', function(err) {
-			if (err) throw err;
-			$.success();
-			$.end();
-		});
 	});
 });
 
