206248aa7c07bfec809d2d739bcf908756392d28
Hanny <hannahparraga@gmail.com>
2020-09-05 17:21:19 -0400

    move flashcards to firebase

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,35 +5,38 @@ import Helmet from 'react-helmet'
 
 export default () => (
 	<div>
-	<Helmet>
-    		<body id={styles.mainbod} />
-	</Helmet>
-	<div id={styles.main}>
-		<h1>Hannah Parraga</h1>
-		<Link to="/flashcards/">
-			Flashcards
+		<Helmet>
+			<body id={styles.mainbod} />
+		</Helmet>
+		<div id={styles.main}>
+			<h1>Hannah Parraga</h1>
+			<a href="https://banu-cards.web.app">
+				Flashcards
+		  </a>
+			<br />
+			<Link to="/translations/">
+				Translations
 		</Link>
-		<br/>
-		<Link to="/translations/">
-			Translations
+			<br />
+			<Link to="/learn/">
+				Programming Tutorials
 		</Link>
-		<br/>	
-		<Link to="/learn/">
-			Programming Tutorials
-		</Link>
-		<br/>
-		<a href="https://milas-icecream-parlour.appspot.com">
-			Mila's Icecream Parlour
+			<br />
+			<a href="https://milas-icecream.web.app/">
+				Mila's Icecream Parlour
 		</a>
-		<br/>
-		<a href="https://github.com/banubot">
-			<img src="github.png" alt=""/>
-		</a>
-		<a href="https://instagram.com/banubot">
-			<img src="insta.png" alt=""/>
-		</a>
-	</div>
-	</div>
+			<br />
+			<a href="https://github.com/banubot">
+				<img src="github.png" alt="" />
+			</a>
+			<a href="https://instagram.com/banubot">
+				<img src="insta.png" alt="" />
+			</a>
+			<a href="https://banubot.tumblr.com/">
+				<img src="tumblr.png" alt="" />
+			</a>
+		</div>
+	</div >
 );
 
 
