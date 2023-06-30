0049818731d3793e3806b56e021eb90b3a0631a5
Hanny <hannahparraga@gmail.com>
2023-06-29 21:55:17 -0500

    add daily

---
+++ daily/script.js
@@ -0,0 +1,72 @@
+import fs;
+var exerciseActivities = fs.readFileSync('exerciseActivities.txt').toString().split("\n");
+for(i in exerciseActivities) {
+    console.log(exerciseActivities[i]);
+}
+
+activities = ["exercise", 'bass', "espanol"];
+espanolOptions = ["read", "write a story", "play w textbook", "vocab cards"]
+bassOptions = ["major scales", "minor scales", "pentatonic scales", "video"]
+exerciseOptions = ["10 desk pushup", "20 curls", "1 min bridge"]
+encouragements = ["proud of you :)", "you've got this :)", "have a great day ☀️", "high five!"]
+
+element = document.getElementById("exerciseBtn");
+element.onclick  = () => open("exercise");
+element = document.getElementById("bassBtn");
+element.onclick  = () => open("bass");
+element = document.getElementById("espanolBtn");
+element.onclick  = () => open("espanol");
+
+var open = function open(which) {
+  for (let i = 0; i < activities.length; i++) {
+    activity = activities[i];
+    if (which != activity) {
+      other = document.getElementById(activity + "Btn");
+      other.style.transition = "opacity 2s linear 0s";
+      other.className = "invisible btn";
+    }
+  } 
+  element = document.getElementById(which + "Btn");
+  element.style.transition = "transform 1s ease 0s";
+  element.style.transform = "translateY(-100px)";
+  element.onclick  = () => close(which);
+
+  list = document.getElementById("list");
+  options = window[which + "Options"]
+  rndI = Math.floor(Math.random() * options.length)
+  list.innerHTML = options[rndI]
+  list.style.transition = "opacity 2s linear 0s";
+  list.className = "list visible"
+  
+  rndI = Math.floor(Math.random() * encouragements.length)
+  encouragement = document.getElementById("encouragements");
+  encouragement.innerHTML = encouragements[rndI]
+  encouragement.style.transition = "opacity 2s linear 0s";
+  encouragement.className = "encouragement visible"
+}
+
+function close(which) {
+  for (let i = 0; i < activities.length; i++) {
+    activity = activities[i];
+    if (which != activity) {
+      other = document.getElementById(activity + "Btn")
+      other.style.transition = "opacity 2s linear 0s";
+      other.className = "btn visible"
+    }
+  } 
+  element = document.getElementById(which + "Btn");
+  element.style.transition = "transform 1s ease 0s";
+  element.style.transform = "translateY(0px)"
+  element.onclick = () => open(which);
+
+  list = document.getElementById("list");
+  list.style.transition = "opacity 2s linear 0s";
+  list.className = "invisible list"
+
+  encouragement = document.getElementById("encouragements");
+  encouragement.style.transition = "opacity 2s linear 0s";
+  encouragement.className = "invisible encouragement"
+}
+
+
+export default open;
