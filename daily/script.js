b2e60b5461b0223dd6d141ecc01ef9a68b688fff
Hanny <hannahparraga@gmail.com>
2023-06-30 00:44:38 -0500

    declare vars

--- daily/script.js
+++ daily/script.js
@@ -1,11 +1,12 @@
 import {exerciseOptions} from './options.js';
-activities = ["exercise", 'bass', "espanol"];
-espanolOptions = ["read", "write a story", "play w textbook", "vocab cards"]
-bassOptions = ["major scales", "minor scales", "pentatonic scales", "video"]
-//exerciseOptions = ["10 desk pushup", "20 curls", "1 min bridge", "https://www.youtube.com/embed/-T9K1HI63GU"]
-encouragements = ["proud of you :)", "you've got this :)", "have a great day ☀️", "high five!"]
 
-element = document.getElementById("exerciseBtn");
+var activities = ["exercise", 'bass', "espanol"];
+var espanolOptions = ["read", "write a story", "play w textbook", "vocab cards"]
+var bassOptions = ["major scales", "minor scales", "pentatonic scales", "video"]
+//exerciseOptions = ["10 desk pushup", "20 curls", "1 min bridge", "https://www.youtube.com/embed/-T9K1HI63GU"]
+var encouragements = ["proud of you :)", "you've got this :)", "have a great day ☀️", "high five!"]
+
+var element = document.getElementById("exerciseBtn");
 element.onclick  = () => open("exercise");
 element = document.getElementById("bassBtn");
 element.onclick  = () => open("bass");
@@ -14,9 +15,9 @@ element.onclick  = () => open("espanol");
 
 var open = function open(which) {
   for (let i = 0; i < activities.length; i++) {
-    activity = activities[i];
+    let activity = activities[i];
     if (which != activity) {
-      other = document.getElementById(activity + "Btn");
+      let other = document.getElementById(activity + "Btn");
       other.style.transition = "opacity 2s linear 0s";
       other.className = "invisible btn";
     }
@@ -26,10 +27,10 @@ var open = function open(which) {
   element.style.transform = "translateY(-100px)";
   element.onclick  = () => close(which);
 
-  list = document.getElementById("list");
-  options = window[which + "Options"]
-  rndI = Math.floor(Math.random() * options.length)
-  option = options[rndI]
+  var list = document.getElementById("list");
+  var options = window[which + "Options"]
+  var rndI = Math.floor(Math.random() * options.length)
+  var option = options[rndI]
   if (option.startsWith('http')) {
     list.innerHTML = '<iframe src="' + option +'" allowfullscreen></iframe>'
   } else {
@@ -38,7 +39,7 @@ var open = function open(which) {
   list.style.transition = "opacity 2s linear 0s";
   list.className = "list visible"
   rndI = Math.floor(Math.random() * encouragements.length)
-  encouragement = document.getElementById("encouragements");
+  var encouragement = document.getElementById("encouragements");
   encouragement.innerHTML = encouragements[rndI]
   encouragement.style.transition = "opacity 2s linear 0s";
   encouragement.className = "encouragement visible"
@@ -46,23 +47,23 @@ var open = function open(which) {
 
 function close(which) {
   for (let i = 0; i < activities.length; i++) {
-    activity = activities[i];
+    var activity = activities[i];
     if (which != activity) {
-      other = document.getElementById(activity + "Btn")
+      var other = document.getElementById(activity + "Btn")
       other.style.transition = "opacity 2s linear 0s";
       other.className = "btn visible"
     }
   } 
-  element = document.getElementById(which + "Btn");
+  var element = document.getElementById(which + "Btn");
   element.style.transition = "transform 1s ease 0s";
   element.style.transform = "translateY(0px)"
   element.onclick = () => open(which);
 
-  list = document.getElementById("list");
+  var list = document.getElementById("list");
   list.style.transition = "opacity 2s linear 0s";
   list.className = "invisible list"
 
-  encouragement = document.getElementById("encouragements");
+  var encouragement = document.getElementById("encouragements");
   encouragement.style.transition = "opacity 2s linear 0s";
   encouragement.className = "invisible encouragement"
 }
